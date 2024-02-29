e83a12eeb130b6d5df4725b1ea0993e0501f0c1c
Viktor <viktor.juhasz@lightware.com>
2024-02-29 22:16:50 +0100

    add basic(smoke) test

---
+++ tests/test_all_with_smoke_setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+Created on 2024.02.29.
+@author: vik1228
+"""
+from utils.functions import generate_numbers_with_builtin, generate_numbers_with_brute_force, generate_numbers_with_backtrack
+
+N = 2
+NUMS = '34'
+
+EXPECTED_LENGTH = 4
+EXPECTED_RESULT = {'33', '34', '43', '44'}
+
+functions = [
+    ("Generate with builtin", generate_numbers_with_builtin),
+    ("Generate with brute force", generate_numbers_with_brute_force),
+    ("Generate with backtrack", generate_numbers_with_backtrack)
+]
+
+for name, function in functions:
+    numbers = function(N, NUMS)
+    print(name, numbers)
+
+    assert set(numbers) == EXPECTED_RESULT, (f"{name}: Generated numbers correct?! "
+                                             f"{set(numbers)} vs {EXPECTED_RESULT}")
+
+    assert len(numbers) == EXPECTED_LENGTH, (f"{name}: Number of generated numbers correct?! "
+                                             f"{len(numbers)} vs {EXPECTED_LENGTH}")
